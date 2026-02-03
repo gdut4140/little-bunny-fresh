7d024c628b2ccdb382d63b51c25cdefa8aaf3b50
ruan-weibo <3037678834@qq.com>
2026-02-03 21:31:54 +0800

    feat:图片懒加载指令的实现

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue'
 import router from './router'
 //引入初始化样式文件
 import '@/styles/common.scss'
+import { useIntersectionObserver } from '@vueuse/core'
 
 const app = createApp(App)
 
@@ -12,3 +13,22 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+//定义全局指令
+app.directive('img-lazy', {
+    mounted(el, binding) {
+        // 创建一个观察对象，来观察当前使用懒加载的元素
+        // <img v-img-lazy="item.picture" alt="">
+        //el:指令绑定的元素img
+        //binding:binding.value 指令等于号后面绑定的表达式的值，图片url
+        useIntersectionObserver(
+            el,
+            ([isIntersecting]) => {
+                if (isIntersecting) {
+                    el.src = binding.value
+                }
+            },
+        )
+
+
+    }
+})
