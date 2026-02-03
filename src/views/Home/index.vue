80020f258be416fd452f5a326881d06f41e1ba78
ruan-weibo <3037678834@qq.com>
2026-02-03 17:35:55 +0800

    feat:实现Home模块的轮播图

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,7 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import HomeProduct from './components/HomeProduct.vue'
+
 </script>
 
 <template>
