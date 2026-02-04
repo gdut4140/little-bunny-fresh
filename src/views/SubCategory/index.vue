4b2124f5258025cbade29e876e08e7f7a239b443
ruan-weibo <3037678834@qq.com>
2026-02-04 21:50:56 +0800

    feat:二级分类基础列表渲染

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -1,7 +1,8 @@
 <script setup>
-import { getCategoryFilterAPI } from '@/apis/category'
+import { getCategoryFilterAPI, getSubCategoryAPI } from '@/apis/category'
 import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
+import GoodsItem from '../Home/components/GoodsItem.vue'
 //获取面包屑数据
 const route = useRoute()
 const categoryData = ref({})
@@ -11,6 +12,21 @@ const getCategoryData = async () => {
 }
 onMounted(() => {
     getCategoryData()
+})
+//获取基础列表数据渲染
+const goodList = ref([])
+const reqData = ref({
+    categoryId: route.params.id,
+    page: 1,
+    pageSize: 20,
+    sortField: 'publishTime'
+})
+const getGoodList = async () => {
+    const res = await getSubCategoryAPI(reqData.value)
+    goodList.value = res.result.items
+}
+onMounted(() => {
+    getGoodList()
 })
 </script>
 
@@ -33,7 +49,7 @@ onMounted(() => {
                 <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
             </el-tabs>
             <div class="body">
-                <!-- 商品列表-->
+                <GoodsItem v-for="good in goodList" :good="good" :key="good.id" />
             </div>
         </div>
     </div>
