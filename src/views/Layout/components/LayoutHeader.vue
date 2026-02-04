11e1958fbdf5a598fe86770cbcee20152f77b390
ruan-weibo <3037678834@qq.com>
2026-02-04 20:41:09 +0800

    feat:二级分类整体认识路由配置

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -11,10 +11,10 @@ const categoryStore = useCategoryStore()
             </h1>
             <ul class="app-header-nav">
                 <li class="home">
-                    <RouterLink to="/">首页</RouterLink>
+                    <RouterLink exact-active-class="active" to="/">首页</RouterLink>
                 </li>
                 <li class="home" v-for="item in categoryStore.categoryList" :key="item.id">
-                    <RouterLink active-class="active" :to="`/category/${item.id}`">{{ item.name }}</RouterLink>
+                    <RouterLink exact-active-class="active" :to="`/category/${item.id}`">{{ item.name }}</RouterLink>
                 </li>
             </ul>
             <div class="search">
