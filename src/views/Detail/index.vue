797c3551353c40f623e892e7092b28777309070f
ruan-weibo <3037678834@qq.com>
2026-02-05 22:11:34 +0800

    feat:详情热榜基础组件封装和基础数据获取渲染

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -1,4 +1,5 @@
 <script setup>
+import DetailHot from './components/DetailHot.vue';
 import { getDetail } from '@/apis/detail';
 import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
@@ -105,7 +106,8 @@ onMounted(() => {
                                 <div class="goods-detail">
                                     <!-- 属性 -->
                                     <ul class="attrs">
-                                        <li v-for="item in goods.details?.properties" :key="item.value">
+                                        <li v-if="goods.details" v-for="item in goods.details.properties"
+                                            :key="item.value">
                                             <span class="dt">{{ item.name }}</span>
                                             <span class="dd">{{ item.value }}</span>
                                         </li>
@@ -117,7 +119,10 @@ onMounted(() => {
                         </div>
                         <!-- 24热榜+专题推荐 -->
                         <div class="goods-aside">
-
+                            <!-- 24小时 -->
+                            <DetailHot />
+                            <!-- 周 -->
+                            <DetailHot />
                         </div>
                     </div>
                 </div>
