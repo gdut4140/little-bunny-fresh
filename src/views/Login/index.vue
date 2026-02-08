2658e6de7043e5bbe5c3fa441ca5e5442fa2ee59
ruan-weibo <3037678834@qq.com>
2026-02-08 23:04:41 +0800

    feat:登录表单校验的自定义校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -4,7 +4,8 @@ import { ref } from 'vue';
 //1.准备表单对象
 const form = ref({
     account: '',
-    password: ''
+    password: '',
+    agree: true
 })
 
 //2.准备规则对象
@@ -15,6 +16,19 @@ const rules = {
     password: [
         { required: true, message: '密码不能为空', trigger: 'blur' },
         { min: 6, max: 14, message: '6到14个字符', trigger: 'blur' }
+    ],
+    agree: [
+        {
+            validator: (rule, value, callback) => {
+                //自定义校验逻辑
+                //勾选就通过 不勾选就不通过
+                if (value) {
+                    callback()
+                } else {
+                    callback(new Error('请勾选协议'))
+                }
+            }
+        }
     ]
 
 }
@@ -49,8 +63,8 @@ const rules = {
                             <el-form-item prop="password" label="密码">
                                 <el-input v-model="form.password" />
                             </el-form-item>
-                            <el-form-item label-width="22px">
-                                <el-checkbox size="large">
+                            <el-form-item prop="agree" label-width="22px">
+                                <el-checkbox size="large" v-model="form.agree">
                                     我已同意隐私条款和服务条款
                                 </el-checkbox>
                             </el-form-item>
