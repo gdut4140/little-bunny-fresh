b9079bbd0eca72ebd4a122518f1a66ef5f9b75f9
ruan-weibo <3037678834@qq.com>
2026-03-19 20:49:07 +0800

    feat:实现本地购物车加入添加

---
+++ src/stores/cartStore.js
@@ -0,0 +1,26 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+export const useCartStore = defineStore('cart', () => {
+    const cartList = ref([])
+    const addCart = (goods) => {
+        //添加购物车
+        //已经添加过count加1
+        //没有添加过直接push
+        //思路：通过匹配传过来的商品对象中的skuId能不能在cartList中找到，找到就是添加过
+        const item = cartList.value.find((cartItem) => goods.skuId === cartItem.skuId)
+        if (item) {
+            // 已经添加过，数量加1
+            item.count += goods.count
+        } else {
+            // 没有添加过，直接push
+            cartList.value.push(goods)
+        }
+    }
+    return {
+        cartList,
+        addCart
+    }
+},
+    {
+        persist: true
+    })
