73e1153ed9bcf602682e13f57503a6bbf6f698f1
ruan-weibo <3037678834@qq.com>
2026-03-21 23:17:18 +0800

    结算页提交订单

--- src/apis/checkout.js
+++ src/apis/checkout.js
@@ -3,4 +3,11 @@ export const getCheckInfoAPI = () => {
     return request({
         url: '/member/order/pre'
     })
-} 
+}
+export const createOrderAPI = (data) => {
+    return request({
+        url: '/member/order',
+        method: 'POST',
+        data
+    })
+}
