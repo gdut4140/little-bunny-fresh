41d2615c058769296f0ad9532fdcad0202e252ab
ruan-weibo <3037678834@qq.com>
2026-03-22 01:10:27 +0800

    倒计时的逻辑函数

---
+++ src/composables/useCountDown.js
@@ -0,0 +1,26 @@
+import { computed, ref } from "vue"
+import dayjs from "dayjs"
+//封装倒计时逻辑函数
+export const useCountDown = () => {
+    //1.响应式数据
+    let timer = null
+    const time = ref(0)
+    //格式化时间为几分几秒
+    const formatTime = computed(() => dayjs.unix(time.value).format('mm分ss秒'))
+    //2.开启倒计时函数
+    const start = (currentTime) => {
+        //核心逻辑编写
+        time.value = currentTime
+        timer = setInterval(() => {
+            time.value--
+        }, 1000)
+    }
+    onUnmounted(() => {
+        timer && clearInterval(timer)
+    })
+    return {
+        formatTime,
+        start,
+    }
+
+}
