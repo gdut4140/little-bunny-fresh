b3e190f7988e07cd4ee97a5a617de83acef2d6ec
ruan-weibo <3037678834@qq.com>
2026-03-21 20:59:17 +0800

    结算页路由配置和基础数据的渲染

--- src/views/CartList/index.vue
+++ src/views/CartList/index.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { useCartStore } from '@/stores/cartStore';
+import router from '@/router';
 const cartStore = useCartStore()
 //单选回调
 const singleCheck = (i, selected) => {
@@ -56,7 +57,7 @@ const singleCheck = (i, selected) => {
                             <td class="tc">
                                 <p>
                                     <el-popconfirm title="确认删除吗?" confirm-button-text="确认" cancel-button-text="取消"
-                                        @confirm="delCart(i)">
+                                        @confirm="cartStore.delCart(i.skuId)">
                                         <template #reference>
                                             <a href="javascript:;">删除</a>
                                         </template>
@@ -84,7 +85,7 @@ const singleCheck = (i, selected) => {
                     <span class="red">¥ {{ cartStore.selectedPrice }} </span>
                 </div>
                 <div class="total">
-                    <el-button size="large" type="primary">下单结算</el-button>
+                    <el-button size="large" type="primary" @click="router.push('/Checkout')">下单结算</el-button>
                 </div>
             </div>
         </div>
